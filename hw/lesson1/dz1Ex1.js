abb0133feda2cc795413586062d363dc74e99adb
Demidov <demidov.evg.v@gmail.com>
2022-02-22 17:00:53 +0300

    demidov_dz1_v2

--- hw/lesson1/dz1Ex1.js
+++ hw/lesson1/dz1Ex1.js
@@ -1,5 +1,10 @@
-`use strict`;
+/*1. Задать температуру в градусах по Цельсию. Вывести в alert соответствующую 
+температуру в градусах по Фаренгейту. Подсказка: расчёт идёт по формуле: 
+Tf = (9 / 5) * Tc + 32, где Tf – температура по Фаренгейту, 
+Tc – температура по Цельсию
+ */
 
+`use strict`;
 function getNumber(message) {
     let number = null;
     while (!number || number === null || isNaN(Number(number))) {
